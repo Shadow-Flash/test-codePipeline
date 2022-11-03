0c0485208aaa21f0aac1258eec5d60cda27e2f85
Shadow-Flash <varun.gupta@sagerx.com>
2022-11-03 16:52:04 +0530

    [ENDED]

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello varun codepipeline noice!",
+            "message": "Codepipeline on Work !!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
