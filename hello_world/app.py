473455e9ba8f4a84005b0c5c85302225d45e693d
Shadow-Flash <varun.gupta@sagerx.com>
2022-11-04 01:07:38 +0530

    [TT 2]

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "TEST 2 !!!",
+            "message": "TEST 3 !!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
